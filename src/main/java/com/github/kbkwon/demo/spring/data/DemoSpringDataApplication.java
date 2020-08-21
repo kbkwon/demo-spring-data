16592dc41d57e0751ccd46efacd318019a48ef1d
Benjamin Kwon <benjamin.kwon.80@gmail.com>
2020-08-21 13:48:55 +0900

    Initial commit

---
+++ src/main/java/com/github/kbkwon/demo/spring/data/DemoSpringDataApplication.java
@@ -0,0 +1,13 @@
+package com.github.kbkwon.demo.spring.data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSpringDataApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/kbkwon/demo/spring/data/DemoSpringDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.kbkwon.demo.spring.data;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringDataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
